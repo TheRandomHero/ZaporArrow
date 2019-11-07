72292a3e2736eb9726cc271aaaf6ab22c34d2fa2
Attila Molnar <scythianhawk@gmai.com>
2019-11-07 15:07:42 +0100

    doesnt find page and action

--- Controllers/ArrowsController.cs
+++ Controllers/ArrowsController.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using ZaporArrow.Models;
 using ZaporArrow.Services;
 
 namespace ZaporArrow.Controllers
@@ -10,11 +12,13 @@ namespace ZaporArrow.Controllers
     public class ArrowsController : Controller
     {
         private readonly IZaporArrowRepository _zaporArrowRepository;
+        private readonly IMapper _mapper;
 
-        public ArrowsController(IZaporArrowRepository arrowRepository)
+        public ArrowsController(IZaporArrowRepository arrowRepository, IMapper mapper)
         {
             _zaporArrowRepository = arrowRepository ??
                 throw new ArgumentException(nameof(arrowRepository));
+            _mapper = mapper;
         }
 
         [HttpGet]
@@ -24,6 +28,14 @@ namespace ZaporArrow.Controllers
             var getAllArrows = _zaporArrowRepository.GetAllArrows();
 
             return View(getAllArrows);
+        }
+
+        [HttpGet("{arrowId:guid}")]
+        public IActionResult ArrowDetails(Guid arrowId)
+        {
+            var arrowFromRepo = _zaporArrowRepository.GetArrow(arrowId);
+
+            return View(arrowFromRepo);
         }
     }
 }
---
+++ Profiles/ArrowsProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ZaporArrow.Profiles
+{
+    public class ArrowsProfile : Profile
+    {
+        public ArrowsProfile()
+        {
+            CreateMap<Entities.Arrow, Models.ArrowDto>()
+                .ForMember(
+                dest => dest.ProfilPicture,
+                opt => opt.MapFrom(src => src.Images.First().ImageSource));
+        }
+    }
+}
